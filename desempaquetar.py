257536fb4a9f9bb2bad0716dd69e66cc6a9b3938
David Cruz <dfcc15@hotmail.com>
2022-03-14 17:03:38 -0500

    Desempaquetar sumatoria en python

---
+++ desempaquetar.py
@@ -0,0 +1,5 @@
+def sumatoria(n1,n2,n3):
+    return n1 + n2 + n3
+numeros = [100, 115,215]
+print(f'La media de {numeros} es {sumatoria (*numeros)}')
+print(f'El promedio es: {sumatoria (*numeros)/3}')
